9878f3c6d1cdaa701a2285cb9a9337d62cd63b98
Pratham Gupta <prathagu@cisco.com>
2024-08-17 18:28:15 +0530

    Basic app setup

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>MIT BLOGPOSTS</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/newform">New Blog</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
