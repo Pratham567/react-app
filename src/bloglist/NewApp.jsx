f1b4bd5b32a7cb49dae86337c8b095baed42081e
Pratham Gupta <prathagu@cisco.com>
2024-08-21 10:30:53 +0530

    Add base code for bloglist

---
+++ src/bloglist/NewApp.jsx
@@ -0,0 +1,66 @@
+import Navbar from '../Navbar';
+import Footer from '../Footer';
+import BlogListDb from './BlogListDb';
+
+import { useState, useEffect } from 'react';
+
+function NewApp() {
+
+  const [blogs, setBlogs] = useState([
+    { title: "Blog Title from component", content: "This is blog component...", author: "Kube Dude", id: 1 }
+  ]);
+
+  // 1. Initialize the blogs state with null, as data to come from DB
+  // 2. Fetch data from API using useEffect
+  // 3. Set the blogs state with the fetched data
+  // 4. Add setTimeout to simulate the delay in fetching data
+  // 5. Add loading state to show loading message while fetching data
+  // 6. Error Handling: Add error state to show error message if data fetching fails
+  // 7. Error Handling: Handle 404 errors
+  // HW: 8. Add a button to fetch data from API
+
+  return (
+    <div className="App">
+      <Navbar />
+      {<BlogListDb blogs={blogs} title={"All blogs from API"} />}
+
+      <Footer />
+    </div>
+  );
+}
+
+export default NewApp;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+useState(() => {
+    setTimeout(() => {
+      fetch(endPoint)
+        .then(res => {
+          return res.json();
+        })
+        .then(data => {
+          console.log(data);
+          setBlogs(data);
+          setLoading(false);
+        });
+    }, 1000);
+  }, []);
+
+*/
